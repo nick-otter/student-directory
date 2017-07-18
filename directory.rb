b0e592e5c63fe5f6cfbb60a9338761e9f7ddacdd
Nick Otter <nicholas.a.otter@gmail.com>
2017-07-18 11:57:44 +0100

    Step 12, loading the data from csv

--- directory.rb
+++ directory.rb
@@ -59,6 +59,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -76,6 +77,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -101,6 +104,17 @@ def save_students
   end
   file.close
 end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  puts @students # to test
+  end
+  file.close
+end
+
 
 interactive_menu
 
